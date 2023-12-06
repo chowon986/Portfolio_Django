d8248796db85fb6a91b6f246ad6cf577268732d6
KimChowon <chowon986@gmail.com>
2023-12-06 21:33:02 +0900

    ✨ URL 별칭

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views # 현재 디렉토리에서 views.py 파일 가져옴
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index), # pybo라는 경로로 요청이 들어오면 views.py index 함수 호출 
-    path('<int:question_id>/', views.detail), # pybo/숫자 경로로 요청이 들어오면 views.py에서 detail 함수 호출
+    path('', views.index, name='index'), # pybo라는 경로로 요청이 들어오면 views.py index 함수 호출 
+    path('<int:question_id>/', views.detail, name='detail'), # pybo/숫자 경로로 요청이 들어오면 views.py에서 detail 함수 호출
 ]
 
