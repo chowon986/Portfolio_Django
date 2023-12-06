83026188a27a98e960dc439061b082ab783845f8
KimChowon <chowon986@gmail.com>
2023-12-06 19:57:56 +0900

    ✨ 장고 앱 migrate, models.py에 모델 정의, 테이블 생성, Question/Answer 생성/조회/수정/삭제

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Field 타입에 관한 URL : https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-types
+
+# Django 모델 정의
+# 한 질문에 여러 개의 답변을 가질 수 있는 일대다 관계
+class Question(models.Model):
+    # 제목처럼 글자수의 길이가 제한된 텍스트는 CharField를 사용
+    subject = models.CharField(max_length=200) 
+    # 길이가 제한되지 않은 텍스트는 TextField를 사용
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+class Answer(models.Model):
+    # ForeignKey는 외래키를 의미하며 다른 모델과 연결하기 위해 사용함
+    # 연결된 Question이 삭제될 때 관련된 Answer들도 함께 삭제되도록 on_delete = models.CASCADE 지정
+    question = models.ForeignKey(Question, on_delete = models.CASCADE) 
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
